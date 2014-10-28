8a2e8b5d24a4e5804d6466763786ef47ba77a750
Harry Baldwin <harrythree@gmail.com>
2014-10-28 01:46:07 -0400

    init

---
+++ socialapp/server.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+app.use(bodyParser.json());
+
+app.get('/api/posts', function(req, res) {
+  res.json([
+    {
+      username: 'harrythree',
+      body: 'node rocks!'
+    }
+  ]);
+});
+
+var Post = require('./models/post');
+app.post('/api/posts', function(req, res, next) {
+  var post = new Post({
+    username: req.body.username,
+    body: req.body.body
+  });
+  post.save(function(err, post) {
+    if (err) { return next(err) }
+    res.status(201).json(post);
+  });
+});
+
+app.listen(3000, function() {
+  console.log('Server listening on', 3000);
+});
